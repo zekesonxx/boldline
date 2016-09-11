4f0c2ca553f701a6ce65ce61ee8d953701bbf064
Zeke Sonxx <zeke@zekesonxx.com>
2016-09-11 17:08:18 -0400

    Better version information output, bump to 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -37,14 +37,24 @@ fn main() {
         }
     };
 
-    let matches = clap_app!(boldline =>
+    let matches: clap::ArgMatches = clap_app!(boldline =>
         (version: crate_version!())
         (about: "Generates a bolded line in a line of repeated text")
         (@setting ArgRequiredElseHelp)
         (@arg marking: -m --marking +takes_value {valid_marking} "Sets the markup to bold with")
         (@arg pattern: -p --pattern +takes_value {valid_pattern} "Sets the pattern of line to draw")
-        (@arg input: +required "The line to bold")
-    ).get_matches();
+        (@group clap_crap =>
+            (@attributes +required)
+            (@arg version: -v --version "Prints version information" )
+            (@arg captialv: -V +hidden)
+            (@arg input: +required "The line to bold")
+        )
+    ).version_short("v").get_matches();
+    if matches.is_present("version") || matches.is_present("captialv") {
+        println!("{}", boldline(concat!("boldline ", crate_version!()).to_string(), Marking::ANSIBold, Pattern::Left).join("\n"));
+        return;
+    }
+
     let input = matches.value_of("input").unwrap().to_string();
     let marking = parse_marking(matches.value_of("marking").unwrap_or("ansi")).unwrap();
     let pattern = parse_pattern(matches.value_of("pattern").unwrap_or("left")).unwrap();
